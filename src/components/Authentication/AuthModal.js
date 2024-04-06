47e06dce685f0f7f75fed6ca120805995b64a4e8
kuldeep-meena <kuldeepmeena567@gmail.com>
2024-04-06 16:54:40 +0530

    removed sign in with Google button

--- src/components/Authentication/AuthModal.js
+++ src/components/Authentication/AuthModal.js
@@ -2,14 +2,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
-import { Button, Tab, Tabs, AppBar, Box } from "@material-ui/core";
+import { Button, Tab, Tabs, AppBar} from "@material-ui/core";
 import Signup from "./Signup";
 import Login from "./Login";
 import { useState } from "react";
-import { CryptoState } from "../../CryptoContext";
-import { auth } from "../../firebase";
-import GoogleButton from "react-google-button";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+//import { CryptoState } from "../../CryptoContext";
+//import { auth } from "../../firebase";
+//import GoogleButton from "react-google-button";
+//import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -38,7 +38,7 @@ export default function AuthModal() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
-  const { setAlert } = CryptoState();
+ // const { setAlert } = CryptoState();
 
   const handleOpen = () => {
     setOpen(true);
@@ -54,28 +54,28 @@ export default function AuthModal() {
     setValue(newValue);
   };
 
-  const googleProvider = new GoogleAuthProvider();
+  //const googleProvider = new GoogleAuthProvider();
 
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, googleProvider)
-      .then((res) => {
-        setAlert({
-          open: true,
-          message: `Sign Up Successful. Welcome ${res.user.email}`,
-          type: "success",
-        });
+  // const signInWithGoogle = () => {
+  //   signInWithPopup(auth, googleProvider)
+  //     .then((res) => {
+  //       setAlert({
+  //         open: true,
+  //         message: `Sign Up Successful. Welcome ${res.user.email}`,
+  //         type: "success",
+  //       });
 
-        handleClose();
-      })
-      .catch((error) => {
-        setAlert({
-          open: true,
-          message: error.message,
-          type: "error",
-        });
-        return;
-      });
-  };
+  //       handleClose();
+  //     })
+  //     .catch((error) => {
+  //       setAlert({
+  //         open: true,
+  //         message: error.message,
+  //         type: "error",
+  //       });
+  //       return;
+  //     });
+  // };
 
   return (
     <div>
@@ -124,13 +124,13 @@ export default function AuthModal() {
             </AppBar>
             {value === 0 && <Login handleClose={handleClose} />}
             {value === 1 && <Signup handleClose={handleClose} />}
-            <Box className={classes.google}>
+            {/* <Box className={classes.google}>
               <span>OR</span>
               <GoogleButton
                 style={{ width: "100%", outline: "none" }}
                 onClick={signInWithGoogle}
               />
-            </Box>
+            </Box> */}
           </div>
         </Fade>
       </Modal>
